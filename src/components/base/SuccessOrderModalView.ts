0c7b965723956098c0a72ce83d99dc14119b66f8
Anna Zelenskaia <anya034@yandex.ru>
2024-11-05 11:28:41 +0100

    Pr9_v20_after review_success

--- src/components/base/SuccessOrderModalView.ts
+++ src/components/base/SuccessOrderModalView.ts
@@ -3,14 +3,15 @@ import { EventEmitter } from './events';
 
 export class SuccessOrderModalView extends Component {
   private eventEmitter: EventEmitter;
+  private descriptionElement: HTMLElement;
 
   constructor(container: HTMLElement, eventEmitter: EventEmitter) {
     super(container);
     this.eventEmitter = eventEmitter;
+    this.descriptionElement = this.container.querySelector('.order-success__description') as HTMLElement;
   }
   
   updateTotal(total: number): void {
-    const descriptionElement = this.container.querySelector('.order-success__description') as HTMLElement;
-    descriptionElement.textContent = `Списано ${total} синапсов`;
+    this.descriptionElement.textContent = `Списано ${total} синапсов`;
   }
 }
